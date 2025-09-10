71d2777bee7b1ed969b071776892790362e70fb6
Lasnine <lasninemiranda@gmail.com>
2025-09-10 10:13:01 -0300

    Fiz a parte 2

---
+++ HtmlCssJava/script.js
@@ -0,0 +1,5 @@
+const Guardar = () => {
+    let guardar = document.getElementById("cor");
+    let quadrado = document.getElementById("quadrado");
+    quadrado.style.backgroundColor = cor.value;
+};
